d03582b81ecbd6f6533f3783210288e57c3e5ff8
PARADOX\brett.scheinert <bscheinert@paradoxplaza.com>
2019-02-25 14:05:31 +0100

    1st commit

---
+++ BattleWank/Source/BattleWank/BattleWankGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleWankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEWANK_API ABattleWankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
